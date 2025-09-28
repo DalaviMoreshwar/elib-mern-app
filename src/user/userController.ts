86ad5988d6420680cfa290b00e2631bf3d80a7ac
Moreshwar Dalavi <moreshwardlv@gmail.com>
2025-09-28 21:12:52 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -55,14 +55,59 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       config.jwtSecret as string,
       {
         expiresIn: "7d",
+        algorithm: "HS256",
       }
     );
 
     // response
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (error) {
     return next(createHttpError(500, "Error while accessing the jwt token"));
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required!"));
+  }
+
+  let user: User | null;
+
+  try {
+    user = await userModel.findOne({ email });
+
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+  } catch (error) {
+    return next(error);
+  }
+
+  try {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(createHttpError(400, "Username or password is incorrect!"));
+    }
+    // jwt token generation
+    const token = sign(
+      {
+        sub: (await user)._id,
+      },
+      config.jwtSecret as string,
+      {
+        expiresIn: "7d",
+        algorithm: "HS256",
+      }
+    );
+
+    // response
+    res.status(200).json({ accessToken: token });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export { createUser, loginUser };
