ea17676d6838ffe49d05ba90bccd07c62f81967a
Moreshwar Dalavi <moreshwardlv@gmail.com>
2025-10-24 23:46:26 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,15 @@ import createHttpError from "http-errors";
 import globalErrorHander from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 
